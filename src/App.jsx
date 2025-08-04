ad118a9cfcd6ebaea6e6de1b9318510bc0b932ac
bitnazari <bitnazari.contact@gmail.com>
2025-08-04 18:26:27 +0200

    CatService ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Gallery from "./components/Gallery";
+import Modal from "./components/Modal";
+import { getCats } from "./services/CatService";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [modalImage, setModalImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCats = async () => {
+    setLoading(true);
+    try {
+      const data = await getCats();
+      setImages(data);
+    } catch (error) {
+      console.error("Error loading cats:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCats();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button
+        onClick={fetchCats}
+        className={`reloadBtn ${loading ? "hidden" : ""}`}
+      >
+        🔄 Refresh Cats
+      </button>
 
-export default App
+      {loading && <div id="loader"></div>}
+
+      {!loading && <Gallery images={images} onImageClick={setModalImage} />}
+
+      <Modal imageUrl={modalImage} onClose={() => setModalImage(null)} />
+    </div>
+  );
+};
+
+export default App;
